b853cd952855d667c088223d5933ca3eab72b512
Ivan Semenov <ivan.semenov@jetbrains.com>
2019-07-16 16:35:07 +0300

    Fix compatibility and relevance with latest IDEA

--- src/org/jetbrains/plugins/iasemenov/gitext/gutterAnnotationActions.kt
+++ src/org/jetbrains/plugins/iasemenov/gitext/gutterAnnotationActions.kt
@@ -2,68 +2,69 @@ package org.jetbrains.plugins.iasemenov.gitext
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.vcs.annotate.AnnotationGutterActionProvider
 import com.intellij.openapi.vcs.annotate.FileAnnotation
 import com.intellij.openapi.vcs.annotate.UpToDateLineNumberListener
-import com.intellij.vcs.log.VcsShortCommitDetails
-import git4idea.GitUtil
-import git4idea.history.GitLogUtil
-
-class FixupByHashGutterActionProvider : AnnotationGutterActionProvider {
-    override fun createAction(annotation: FileAnnotation): AnAction {
-        return object : AnAction("Fixup by hash"), UpToDateLineNumberListener {
-            private var myLineNumber = -1
-
-            override fun actionPerformed(e: AnActionEvent) {
-                val project = e.dataContext.getData(CommonDataKeys.PROJECT) ?: return
-                val file = e.dataContext.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
-
-                FileDocumentManager.getInstance().getDocument(file) ?: return
-                GitUtil.getRepositoryManager(project).getRepositoryForFileQuick(file) ?: return
-
-                val revisionHash = annotation.getLineRevisionNumber(myLineNumber)?.asString() ?: return
-
-                commitFixup(project, file, revisionHash)
-            }
-
-            override fun consume(integer: Int) {
-                myLineNumber = integer
-            }
-        }
-    }
-}
+import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.ChangesUtil
+import com.intellij.openapi.vcs.changes.ui.CommitChangeListDialog
+import com.intellij.vcs.log.VcsCommitMetadata
+import git4idea.history.GitHistoryUtils
+import git4idea.rebase.GitAutoSquashCommitAction
+import git4idea.rebase.GitCommitEditingAction
+import git4idea.repo.GitRepositoryManager
 
 class FixupBySubjectGutterActionProvider : AnnotationGutterActionProvider {
     override fun createAction(annotation: FileAnnotation): AnAction {
-        return object : AnAction("Fixup by subject"), UpToDateLineNumberListener {
+        return object : GitCommitEditingAction(), UpToDateLineNumberListener {
             private var myLineNumber = -1
 
-            override fun actionPerformed(e: AnActionEvent) {
-                val project = e.dataContext.getData(CommonDataKeys.PROJECT) ?: return
-                val file = e.dataContext.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
-
-                FileDocumentManager.getInstance().getDocument(file) ?: return
-                val repository = GitUtil.getRepositoryManager(project).getRepositoryForFileQuick(file) ?: return
+            override fun actionPerformedAfterChecks(e: AnActionEvent) {
+                val project = e.project!!
 
                 val revisionHash = annotation.getLineRevisionNumber(myLineNumber)?.asString() ?: return
 
+                val changeList = ChangeListManager.getInstance(project).defaultChangeList
+                val repository = getRepository(e)
+
                 object : Task.Modal(project, "Getting commit details", true) {
-                    private lateinit var details: List<VcsShortCommitDetails>
+                    private lateinit var details: List<VcsCommitMetadata>
 
                     override fun run(indicator: ProgressIndicator) {
-                        details = GitLogUtil.collectShortDetails(project, repository.vcs, repository.root, listOf(revisionHash))
+                        details = GitHistoryUtils.collectCommitsMetadata(project, repository.root, revisionHash)
+                                ?: throw ProcessCanceledException()
                     }
 
                     override fun onSuccess() {
                         if (details.isEmpty()) return
-                        commitFixup(project, file, details.first().subject)
+
+                        val gitRepositoryManager = GitRepositoryManager.getInstance(project)
+
+                        val changes = changeList.changes.filter {
+                            gitRepositoryManager.getRepositoryForFile(ChangesUtil.getFilePath(it)) == repository
+                        }
+
+                        val executors = repository.vcs.commitExecutors + if (getProhibitedStateMessage(e, "rebase") == null)
+                            listOf(GitAutoSquashCommitAction.GitRebaseAfterCommitExecutor(project, repository, "$revisionHash~"))
+                        else listOf()
+                        CommitChangeListDialog.commitChanges(project,
+                                changes,
+                                changes,
+                                changeList,
+                                executors,
+                                true,
+                                null,
+                                "fixup! ${details.first().subject}",
+                                null,
+                                true)
                     }
                 }.queue()
             }
+
+            override fun getFailureTitle() = "Can't Create Fixup Commit"
 
             override fun consume(integer: Int) {
                 myLineNumber = integer
--- src/org/jetbrains/plugins/iasemenov/gitext/logActions.kt
+++ src/org/jetbrains/plugins/iasemenov/gitext/logActions.kt
@@ -5,26 +5,11 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.vcs.log.VcsFullCommitDetails
 import git4idea.branch.GitRebaseParams
+import git4idea.config.GitExecutableManager
 import git4idea.rebase.GitCommitEditingAction
 import git4idea.rebase.GitInteractiveRebaseEditorHandler
-import git4idea.rebase.GitRebaseEditorService
 import git4idea.rebase.GitRebaseUtils
-import git4idea.repo.GitRepository
-import git4idea.reset.GitOneCommitPerRepoLogAction
-
-class FixupByHashLogAction : GitOneCommitPerRepoLogAction() {
-    override fun actionPerformed(project: Project, commits: Map<GitRepository, VcsFullCommitDetails>) {
-        commitFixup(project, commits.values.first().id.asString())
-    }
-}
-
-class FixupBySubjectLogAction : GitOneCommitPerRepoLogAction() {
-    override fun actionPerformed(project: Project, commits: Map<GitRepository, VcsFullCommitDetails>) {
-        commitFixup(project, commits.values.first().subject)
-    }
-}
 
 class GitInteractiveAutoRebaseAction : GitCommitEditingAction() {
     override fun update(e: AnActionEvent) {
@@ -35,12 +20,14 @@ class GitInteractiveAutoRebaseAction : GitCommitEditingAction() {
     override fun actionPerformedAfterChecks(e: AnActionEvent) {
         val commit = getSelectedCommit(e)
         val project = e.project!!
+        val gitVersion = GitExecutableManager.getInstance().getVersion(project)
+
         val repository = getRepository(e)
 
         object : Task.Backgroundable(project, "Rebasing") {
             override fun run(indicator: ProgressIndicator) {
                 val editor = MyRebaseEditor(project, repository.root)
-                val params = GitRebaseParams.editCommits(commit.parents.first().asString(), editor, false)
+                val params = GitRebaseParams.editCommits(gitVersion, commit.parents.first().asString(), editor, false)
                 GitRebaseUtils.rebase(project, listOf(repository), params, indicator)
             }
         }.queue()
@@ -49,7 +36,7 @@ class GitInteractiveAutoRebaseAction : GitCommitEditingAction() {
     override fun getFailureTitle() = "Couldn't Start Rebase"
 
     class MyRebaseEditor(project: Project, root: VirtualFile)
-        : GitInteractiveRebaseEditorHandler(GitRebaseEditorService.getInstance(), project, root) {
+        : GitInteractiveRebaseEditorHandler(project, root) {
         override fun handleInteractiveEditor(path: String) = true
     }
 }
